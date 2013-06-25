95398ca000829ba7fdf6727f45e75311b5a98d88
leonn <leonn.paiva@gmail.com>
2013-06-25 18:09:56 -0300

    tool modularization

--- lattesData.php
+++ lattesData.php
@@ -1,19 +1,20 @@
 <?php
-require_once('lattesUtil.php');
-$link_lattes="http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=";
 
-error_reporting(E_ALL | E_ERROR );
-ini_set('default_charset','UTF-8');
 
 if(isset($_POST['data'])){
+	require_once('lattesUtil.php');
+	$link_lattes="http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=";
 
-  $link_lattes=$link_lattes.$_POST['data'];
-  $html=lattesUtil::carregaLattes($link_lattes);
-  $txtInf = lattesUtil::textoInformadoAutor($html);
-  $nivel=explode('- ', $txtInf);
-  $nivel=$nivel[1];
+	error_reporting(E_ALL | E_ERROR );
+	ini_set('default_charset','UTF-8');
 
-  echo $link_lattes.','.$txtInf.','.$nivel;
-  }
-  
+	$link_lattes=$link_lattes.$_POST['data'];
+	$html=lattesUtil::carregaLattes($link_lattes);
+	$txtInf = lattesUtil::textoInformadoAutor($html);
+	$nivel=explode('- ', $txtInf);
+	$nivel=$nivel[1];
+
+	echo $link_lattes.','.$txtInf.','.$nivel;
+}
+
 ?>
